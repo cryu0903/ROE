56f5475abafc51bbae93cb97f3ab873b52544108
cryu0903 <clairejiwooryu@gmail.com>
2022-06-16 10:58:48 -0400

    finished learn more

--- ROE/FurtherInfoViewController.swift
+++ ROE/FurtherInfoViewController.swift
@@ -14,7 +14,13 @@ class FurtherInfoViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func goToClinic(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToInClinic", sender: self)
+    }
     
-
+    @IBAction func goToPill(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToPill", sender: self)
+    }
+    
 
 }
--- ROE/RoeVWadeViewController.swift
+++ ROE/RoeVWadeViewController.swift
@@ -6,24 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class RoeVWadeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToState(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.plannedparenthoodaction.org/abortion-access-tool/US")! as URL,options: [:], completionHandler:nil )
     }
-    */
+    
+    
+    
 
 }
