00090a048bf7abba80d204c668fdf38dc16be47c
cryu0903 <clairejiwooryu@gmail.com>
2022-06-16 10:17:50 -0400

    updates to abortion info

--- ROE/AbortionInfoViewController.swift
+++ ROE/AbortionInfoViewController.swift
@@ -15,15 +15,14 @@ class AbortionInfoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToRoe(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToRoe", sender: self)
     }
-    */
+    
+    
+    @IBAction func goToFurther(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToFurtherInfo", sender: self)
+    }
+    
 
 }
