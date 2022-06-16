73c364faba5d824cc558524316cefbbed756fcfa
Azshalay Utley <azshalayutley08@gmail.com>
2022-06-16 00:48:55 -0400

    updated map source code

--- ROE/MapViewController.swift
+++ ROE/MapViewController.swift
@@ -8,14 +8,16 @@
 import MapKit
 import SwiftUI
 
+
 struct MapView: View {
     @StateObject private var viewModel = MapViewModel()
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.2271, longitude: 80.8431),
-                                                                                  span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true)
+        Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
             .ignoresSafeArea()
+            .onAppear{
+                viewModel.checkIfLocationServiciceIsEnabled()
+            }
     
     }
 }
@@ -25,9 +27,46 @@ struct ContentView_Previews: PreviewProvider{
     }
     
 }
-final class MapViewModel: ObservableObject {
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     
-    var locationManager: CLLocationManager
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.2271, longitude: 80.8431),
+                                                                                  span: MKCoordinateSpan(latitudeDelta: 0.30, longitudeDelta: 0.30))
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServiciceIsEnabled(){
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager = CLLocationManager()
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager?.delegate = self
+
+        } else {
+            print("Your location is currently not enabaled. Please enable in order to show the availabilty of services in your area.")
+        }
+        
+    }
+    private func checkLocationAuthorization(){
+        guard let locationMangager = locationManager else {return}
+        
+        switch locationManager?.authorizationStatus{
+            
+        case .notDetermined, .none:
+            locationMangager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely due to parental controls.")
+        case .denied:
+            print("Location denied. Go into setting to change app permissions.")
+        case . authorizedAlways,. authorizedWhenInUse:
+            region = MKCoordinateRegion(center: locationMangager.location!.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        
+        @unknown default:
+            break
+            
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
     
 }
  
